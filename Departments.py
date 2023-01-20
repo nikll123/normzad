629ed05ec1ab2ee67229eb069c4476f7eb6c127a
nikll123@hotmail.com <nikll123@hotmail.com>
2023-01-20 14:01:25 +0200

    delete added

--- Departments.py
+++ Departments.py
@@ -5,25 +5,42 @@ import db
 
 tableName = "Departments"
 
-def New(name):
+def new(name):
     name = name.strip()
     err = config.dummyErr
-    newId = config.dummyNum
+    newId = None
     if name:
         with sqlite3.connect(config.dbFileName) as conn:
             cursor = conn.cursor()
-            sql = f"""INSERT INTO {tableName} (Name) VALUES ("{name}")"""
+            sql = f"""INSERT INTO {tableName} (Name) VALUES (?)"""
             try:
-                cursor.execute(sql)
+                cursor.execute(sql, [name])
                 newId = db.getLastId(cursor)
                 err = ''
             except sqlite3.IntegrityError:
                 err = f"Дублирование данных: '{name}'"
-                
+            except Exception as ex: 
+                err = f"SQL eror:\n   {sql}\n   {ex.args[0]}"
     else:
         err = "Нет данных"
     return err, newId
 
+def delete(id):
+    err = config.dummyErr
+    with sqlite3.connect(config.dbFileName) as conn:
+        cursor = conn.cursor()
+        sql = f"""DELETE FROM {tableName} WHERE ID=?"""
+        try:
+            cursor.execute(sql, [id])
+            err = ''
+        except Exception as ex: 
+            err = f"SQL eror:\n   {sql}\n   {ex.args[0]}"
+    return err
+
 
 if __name__ == '__main__':
-    print (New("ttt3"))
+    print (new("ttt4"))
+
+    print (delete(6))
+
+    pass
