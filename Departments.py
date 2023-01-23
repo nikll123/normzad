c9fb061c2b93a4285c9b56381219abba8ad29fde
nikll123@hotmail.com <nikll123@hotmail.com>
2023-01-23 21:47:22 +0200

    refactoring

--- Departments.py
+++ Departments.py
@@ -5,26 +5,26 @@ tableName = 'Departments'
 
 def new(name):
     newId = None
-    err = "Нет данных"
     name = name.strip()
     if name:
         sql = f"""INSERT INTO {tableName} (Name) VALUES (?)"""
         err, newId = db.executeTry(sql, [name])
+    else:
+        err = "Нет данных"
     return err, newId
-
 
 def delete(id):
     sql = f"""DELETE FROM {tableName} WHERE ID=?"""
     err, _notUsed = db.executeTry(sql, [id])
     return err
 
-
 def update(id, name):
-    err = "Нет данных"
     name = name.strip()
     if name:
         sql = f"""UPDATE {tableName} SET name=? WHERE ID=?"""
         err, _notUsed = db.executeTry(sql, [name, id])
+    else:
+        err = "Нет данных"
     return err
 
 
