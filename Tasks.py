efb2677b78cd4be01dd128748bbe9920123fb58c
nikll123@hotmail.com <nikll123@hotmail.com>
2023-01-24 11:54:08 +0200

    tasks added

---
+++ Tasks.py
@@ -0,0 +1,42 @@
+# Tasks stuff
+import db
+
+tableName = 'Tasks'
+pkField = 'id'
+
+def new(newName):
+    newName = newName.strip()
+    if newName:
+        fldList = ['Name']
+        err, newId  = db.insert(tableName, fldList, [newName])
+    else:
+        newId = None
+        err = "Нет данных"
+    return err, newId
+
+def delete(id):
+    err = db.delete(tableName, pkField, id)
+    return err
+
+def update(id, newName):
+    newName = newName.strip()
+    if newName:
+        fldList = ['Name']
+        err = db.update(tableName, fldList, [newName], pkField, id)
+    else:
+        err = "Нет данных"
+    return err
+
+
+if __name__ == '__main__':
+    err, idTest = new("Tasks Test")
+    print (err, idTest)
+
+    if idTest != None:
+        err = update(idTest, "Tasks Test new name")
+        print (err)
+
+        err = delete(idTest)
+        print (err)
+
+
